7511528ae229ee87acca01456c436ccacd99e6b9
Davi <davi.zelski@gmail.com>
2025-02-18 15:19:32 -0300

    input

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/button";
+import { ArrowRight, Copy } from "lucide-react";
+import { IconButton } from "@/components/icon-button";
+import { Input } from "@/components/input";
+
+export default function Home() {
+  return (
+    <main>
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <Input type = "email" placeholder="Digite seu e-mail"/>
+      </div>
+    </main>
+  );
+}
